463b60958bdb595f0b8c10a8a183ea7d6a87d887
Dhruvkumar Maisuria <dhruvmaisuriya123@gmail.com>
2024-06-14 10:58:23 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -11,17 +11,68 @@ console.log(typeof valueinNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log("BooleanIsLoggedIn "+booleanIsLoggedIn);
+console.log("BooleanIsLoggedIn " + booleanIsLoggedIn);
 
-let isLoggedout = 0  
+let isLoggedout = 0
 let booleanIsLoggedout = Boolean(isLoggedout)
-console.log("BooleanIsLoggedout "+booleanIsLoggedout);
+console.log("BooleanIsLoggedout " + booleanIsLoggedout);
 
 // 0,"" => false
 // 1,"dhruv"=> true  
 
 
-let somenumber =  9099
+let somenumber = 9099
 let stringmaker = String(somenumber);
 console.log(typeof stringmaker)
 console.log(stringmaker);
+
+
+
+
+
+//  ************************************* Operations ***************************************************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 ="Dhruv"
+let str2 =" Maisuria"
+let str3 =str1 + str2
+console.log(str3);
+
+
+console.log(1+"2");
+console.log("2"+ 1);
+console.log("1"+2+3);
+
+
+console.log(1+2+"3");
+console.log(3*5+5-8/5); //not write like this
+
+console.log([(3*5)+1-(4/2)]);
+
+// 
+console.log(+true);
+
+// 
+let num1,num2,num3 
+num1=num2=num3 = 2+4
+console.log(num1)
+
+// incremental assignment prefix and postfix
+
+let x =3
+let y = x++;
+console.log(`x:${x} , y:${y}`);
+
+let a =3
+let b = ++a;
+console.log(`a:${a} , b:${b}`);
