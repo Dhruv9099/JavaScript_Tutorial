168ba6c84cd7bf60b2d2910c771ccdbd6867d402
Dhruvkumar Maisuria <dhruvmaisuriya123@gmail.com>
2024-06-14 10:32:38 +0000

    converion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"; // string
+console.log(typeof score);
+
+let valueinNumber = Number(score); // conversion 
+console.log(typeof valueinNumber);
+
+//   "33 => 33 "
+// "33abc => NaN"
+// 
+// true=> 1 ;  false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log("BooleanIsLoggedIn "+booleanIsLoggedIn);
+
+let isLoggedout = 0  
+let booleanIsLoggedout = Boolean(isLoggedout)
+console.log("BooleanIsLoggedout "+booleanIsLoggedout);
+
+// 0,"" => false
+// 1,"dhruv"=> true  
+
+
+let somenumber =  9099
+let stringmaker = String(somenumber);
+console.log(typeof stringmaker)
+console.log(stringmaker);
